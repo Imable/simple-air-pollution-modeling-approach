bcde18d92252bfe47da4e3b8d54af767c618ab0c
Abel <abelvanbeek@hotmail.com>
2020-06-15 16:53:47 +0200

    Added default arguments

--- pre/arguments.py
+++ pre/arguments.py
@@ -4,23 +4,28 @@ from datetime import datetime
 ARGUMENTS = {
     'start_ts': {
         'help': 'Date of the start of calculation timeframe in DD-MM-YYYY format',
-        'parse': (lambda x: datetime.strptime(x, '%d-%m-%Y'))
+        'parse': (lambda x: datetime.strptime(x, '%d-%m-%Y')),
+        'default': 'No start date provided'
     },
     'end_ts': {
         'help': 'Date of the end of calculation timeframe in DD-MM-YYYY format',
-        'parse': (lambda x: datetime.strptime(x, '%d-%m-%Y')) 
+        'parse': (lambda x: datetime.strptime(x, '%d-%m-%Y')),
+        'default': 'No end date provided'
     },
     'step': {
         'help': 'Step size for the model interval in minutes (M)',
-        'parse': (lambda x: int(x))  
+        'parse': (lambda x: int(x)),
+        'default': '60'
     },
     'raster': {
         'help': 'Filepath of the file containing the digital elevation model',
-        'parse': (lambda x: x)
+        'parse': (lambda x: x),
+        'default': 'No raster provided'
     },
     'inversion_layer': {
         'help': 'Inversion layer height in meters (m)',
-        'parse': (lambda x: int(x))
+        'parse': (lambda x: int(x)),
+        'default': 'No inversion layer height provided'
     }
     # 'validation_data': 'Filepath of a file including own measured concentration data in CSV format',
     # 'source_data': 'Filepath of a file including data of pollution sources',
@@ -32,10 +37,10 @@ class Arguments():
         self.reader = argparse.ArgumentParser()
 
         for tag, props in ARGUMENTS.items():
-            self.add(tag, props['help'])
+            self.add(tag, props['help'], props['default'])
 
-    def add(self, tag, help):
-        self.reader.add_argument(f'--{tag}', help=help)
+    def add(self, tag, help, default):
+        self.reader.add_argument(f'--{tag}', help=help, default=default)
 
     def get(self):
         args = vars(self.reader.parse_args())
