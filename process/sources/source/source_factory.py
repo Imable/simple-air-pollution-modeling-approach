01dbf935aefc2bdd44b2b3e8d0c0ceb7e3c28bcb
Abel <abelvanbeek@hotmail.com>
2020-08-08 14:18:42 +0200

    Removed Hurtigruten

--- process/sources/source/source_factory.py
+++ process/sources/source/source_factory.py
@@ -17,33 +17,34 @@ class SourceFactory:
         # }
 
         return [
-            ScheduledShip(
-                {
-                    ('01-06-2014', '31-08-2014'): [
-                        '14:00'
-                    ],
-                    ('01-06-2015', '31-08-2015'): [
-                        '14:00'
-                    ],
-                    ('01-06-2016', '31-08-2016'): [
-                        '14:00'
-                    ],
-                    ('01-06-2017', '31-08-2017'): [
-                        '14:00'
-                    ],
-                    ('01-06-2018', '31-08-2018'): [
-                        '14:00'
-                    ],
-                    ('01-06-2019', '31-08-2019'): [
-                        '14:00'
-                    ]
-                },
-                timedelta(minutes=20), # Harbour time
-                datetime.now(),
-                timedelta(minutes=20), # Manouvering time
-                'Hurtigruten',
-                debug
-            ),
+            # Commented out because Hurtigruten is already included in port schedule
+            # ScheduledShip(
+            #     {
+            #         ('01-06-2014', '31-08-2014'): [
+            #             '14:00'
+            #         ],
+            #         ('01-06-2015', '31-08-2015'): [
+            #             '14:00'
+            #         ],
+            #         ('01-06-2016', '31-08-2016'): [
+            #             '14:00'
+            #         ],
+            #         ('01-06-2017', '31-08-2017'): [
+            #             '14:00'
+            #         ],
+            #         ('01-06-2018', '31-08-2018'): [
+            #             '14:00'
+            #         ],
+            #         ('01-06-2019', '31-08-2019'): [
+            #             '14:00'
+            #         ]
+            #     },
+            #     timedelta(minutes=20), # Harbour time
+            #     datetime.now(),
+            #     timedelta(minutes=20), # Manouvering time
+            #     'Hurtigruten',
+            #     debug
+            # ),
             ScheduledShip(
                 {
                     ('01-05-2016', '11-08-2016'): [
