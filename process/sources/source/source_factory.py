6eb91375e24a9f446bf67fbd2fa3090fba32d234
Abel <abelvanbeek@hotmail.com>
2021-04-03 13:50:03 +0200

    Added explicit date format for ship schedule has fixed an ordering issue causing the model to give faulty results

--- process/sources/source/source_factory.py
+++ process/sources/source/source_factory.py
@@ -48,66 +48,66 @@ class SourceFactory:
             ScheduledShip(
                 {
                     ('01-05-2015', '31-05-2015'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-06-2015', '31-08-2015'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '8:05',
-                        '9:35',
-                        '11:05',
-                        '12:35',
-                        '14:05',
-                        '15:35',
-                        '17:05',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '7:55',
+                        '9:25',
+                        '10:55',
+                        '12:25',
+                        '13:55',
+                        '15:25',
+                        '16:55',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-09-2015', '30-09-2015'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-10-2015', '31-10-2015'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-05-2016', '31-05-2016'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-06-2016', '31-08-2016'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '8:05',
-                        '9:35',
-                        '11:05',
-                        '12:35',
-                        '14:05',
-                        '15:35',
-                        '17:05',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '7:55',
+                        '9:25',
+                        '10:55',
+                        '12:25',
+                        '13:55',
+                        '15:25',
+                        '16:55',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-09-2016', '30-09-2016'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-10-2016', '31-10-2016'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25' # It's actually not departing here, but the morning after...
                     ],
                     # What we originally had for 2016
                     # ('01-05-2016', '11-08-2016'): [
@@ -118,116 +118,116 @@ class SourceFactory:
                     #     '18:35' # It's actually not departing here, but the nmorning after...
                     # ],
                     ('01-05-2017', '31-05-2017'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-06-2017', '31-08-2017'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '8:05',
-                        '9:35',
-                        '11:05',
-                        '12:35',
-                        '14:05',
-                        '15:35',
-                        '17:05',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '7:55',
+                        '9:25',
+                        '10:55',
+                        '12:25',
+                        '13:55',
+                        '15:25',
+                        '16:55',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-09-2017', '30-09-2017'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-10-2017', '31-10-2017'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-04-2018', '30-04-2018'): [
-                        '7:05', # It's actually not arriving here, but the night before...
-                        '10:35',
-                        '13:35',
-                        '17:35' # It's actually not departing here, but the morning after...
+                        '6:55', # It's actually not arriving here, but the night before...
+                        '10:25',
+                        '13:25',
+                        '17:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-05-2018', '19-05-2018'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('20-05-2018', '10-09-2018'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '8:05',
-                        '9:35',
-                        '11:05',
-                        '12:35',
-                        '14:05',
-                        '15:35',
-                        '17:05',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '7:55',
+                        '9:25',
+                        '10:55',
+                        '12:25',
+                        '13:55',
+                        '15:25',
+                        '16:55',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('11-09-2018', '30-09-2018'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-10-2018', '31-10-2018'): [
-                        '7:05', # It's actually not arriving here, but the night before...
-                        '10:35',
-                        '13:35',
-                        '17:35' # It's actually not departing here, but the morning after...
+                        '6:55', # It's actually not arriving here, but the night before...
+                        '10:25',
+                        '13:25',
+                        '17:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-04-2019', '30-04-2019'): [
-                        '7:05', # It's actually not arriving here, but the night before...
-                        '11:35',
-                        '14:35',
-                        '17:35' # It's actually not departing here, but the morning after...
+                        '7:55', # It's actually not arriving here, but the night before...
+                        '11:25',
+                        '14:25',
+                        '17:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-05-2019', '19-05-2019'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('20-05-2019', '10-09-2019'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '8:05',
-                        '9:35',
-                        '11:05',
-                        '12:35',
-                        '14:05',
-                        '15:35',
-                        '17:05',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '7:55',
+                        '9:25',
+                        '10:55',
+                        '12:25',
+                        '13:55',
+                        '15:25',
+                        '16:55',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('11-09-2019', '30-09-2019'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                     ('01-10-2019', '31-10-2019'): [
-                        '8:05', # It's actually not arriving here, but the night before...
-                        '11:35',
-                        '14:35',
-                        '17:35' # It's actually not departing here, but the morning after...
+                        '7:55', # It's actually not arriving here, but the night before...
+                        '11:25',
+                        '14:25',
+                        '17:25' # It's actually not departing here, but the morning after...
                     ],
                     ('12-06-2020', '30-09-2020'): [
-                        '6:35', # It's actually not arriving here, but the night before...
-                        '9:35',
-                        '12:35',
-                        '15:35',
-                        '18:35' # It's actually not departing here, but the morning after...
+                        '6:25', # It's actually not arriving here, but the night before...
+                        '9:25',
+                        '12:25',
+                        '15:25',
+                        '18:25' # It's actually not departing here, but the morning after...
                     ],
                 },
               timedelta(minutes=25), # Harbour time
